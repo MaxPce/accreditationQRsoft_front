61c1810002e419c3ba895a3f9842cb82ad890935
MaxPce <max.ponce@tecsup.edu.pe>
2026-07-09 12:20:28 -0500

    9-7-3

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,21 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    basicSsl()
+    basicSsl(),
   ],
   server: {
-    host: true,   
+    host: true,
     port: 5173,
-    
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
